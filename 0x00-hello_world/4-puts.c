c9b1234e53fb261727b26bd428e50a7e33405e59
Ahmed sherif mohamed <ahmed_sherif619@yahoo.com>
2023-08-10 17:50:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,10 +7,8 @@
 int main(void)
 {
 
-	char alx[]="\"Programming is like building a multilingual puzzle";
+	char alx[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(alx);
-
-
-    		return (0);
+    	return (0);
 }
