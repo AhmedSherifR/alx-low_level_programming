319796de4ea9910be1b2e33c28414ec1f798fee8
Ahmed sherif mohamed <ahmed_sherif619@yahoo.com>
2023-08-15 17:50:38 -0700

    print_alphabet_x10 - prints alphabet letters

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * void print_alphabet_x10(void)
- * print lowercase alphabet 10 times
+ * print_alphabet_x10 - prints alphabet letters ten times
+ * 
  *
  * Return: void (Success)
  */
 void print_alphabet_x10(void)
 {
-	int i = 97 , j = 1;
+	int i = 97, j = 1;
 
-	while(j < 11)
+	while (j < 11)
 	{
-		while(i < 123 )
+		while (i < 123)
 		{
 		_putchar(i);
 		++i;
 		}
 	_putchar(10);
-	i =97;
+	i = 97;
 
 	++j;
 	}
