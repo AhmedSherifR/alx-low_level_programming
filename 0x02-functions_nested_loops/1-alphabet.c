107b970111574534eebfdc475f63e6056bcc90c7
Ahmed sherif mohamed <ahmed_sherif619@yahoo.com>
2023-08-15 17:27:08 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,17 @@
 /**
  *print_alphabet - function that print alphabet in lowercase
  *
- *No return 
+ *No return
  */
 void print_alphabet(void)
 {
-	int i = 97 ;
+	int i = 97;
 
-	while(i < 123 )
-       	{
-		_putchar(i);
-		++i;
-	}
+	while (i < 123)
+	{
+	_putchar(i);
+	++i;
+	}	
 
 
 	_putchar(10);
