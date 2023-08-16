7eb1cb71987b5a83e29787b869304d63e80abf52
Ahmed sherif mohamed <ahmed_sherif619@yahoo.com>
2023-08-15 18:54:09 -0700

    #include main.h /** * _abs - prints the absolute value of a number *@i: number targeted *Return: returns 0 */

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _abs - prints the absolute value of a number
+*@i: number targeted
+*Return: returns 0
+*/
+int _abs(int i)
+{
+	if (i > 0) return (i);
+	if (i < 0) return (i * -1);
+
+	return (0);
+}
