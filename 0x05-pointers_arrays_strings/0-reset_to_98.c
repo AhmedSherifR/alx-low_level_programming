6d863e0fc5dae33b9e7871a54686a6353dc1d1f3
Ahmed sherif mohamed <ahmed_sherif619@yahoo.com>
2023-08-21 17:17:01 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ * @n: pointer to the integer variable to update
+ * return - no output
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
