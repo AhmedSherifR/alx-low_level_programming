48548f19353c01d6a641d57c1a579da990732340
Ahmed sherif mohamed <ahmed_sherif619@yahoo.com>
2023-08-21 17:22:50 -0700

    void swap_int(int *a, int *b)

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap values of two integers
+ * @a - pointer to the first integer argument
+ * @b - pointer to the second integer argument
+ * return - no output
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
