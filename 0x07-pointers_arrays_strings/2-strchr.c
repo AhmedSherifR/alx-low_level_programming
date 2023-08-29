93b2a828c9c80fdf86107ece57eafdc3b43f6b66
Ahmed sherif mohamed <ahmed_sherif619@yahoo.com>
2023-08-28 17:26:40 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character in a string
+ *@s: first value -char
+ *@c: second value - char
+ *
+ * Return: char with result
+ */
+char *_strchr(char *s, char c)
+{
+	if (c == '\0')
+		return (s);
+
+	while (*s != '\0')
+	{
+	if (*s == c)
+		{
+		return (s);
+		}
+	*s++;
+	}
+	return (NULL);
+}
+
