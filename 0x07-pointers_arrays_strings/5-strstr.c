36fc84da775aa8def2208fa63b547a4359b4dbd1
Ahmed sherif mohamed <ahmed_sherif619@yahoo.com>
2023-08-28 18:39:09 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strstr - function that locates a substring.
+ *@haystack: first value -char
+ *@needle: second value - char
+ *
+ * Return: char with result
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+	register char *a, *b;
+
+	b = needle;
+
+	if (*b == 0)
+		return (haystack);
+
+	for ( ; *haystack != 0; haystack += 1)
+	{
+	if (*haystack != *b)
+		continue;
+
+	a = haystack;
+	while (1)
+	{
+	if (*b == 0)
+		return (haystack);
+
+	if (*a++ != *b++)
+		break
+	}
+	b = needle;
+	}
+	return (NULL);
+}
+
+
+
