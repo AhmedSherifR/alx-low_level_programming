ef5a1e4583018be3a470c2196bac95f157b36897
Ahmed sherif mohamed <ahmed_sherif619@yahoo.com>
2023-09-14 19:02:03 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,10 @@
-#ifdef VAR_Func
-#define VAR_Func
-#include <stdarg.h>
-#include <stdlib.h>
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _putchar(char c);
 
 #endif
-
