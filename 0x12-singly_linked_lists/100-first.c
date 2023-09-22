41cd00eacff9d94eaa9e547e59f0e6c159a7c8f9
AhmedSherifR <ahmed_sherif619@yahoo.com>
2023-09-21 19:20:22 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
+
+void before_main(void) __attribute__((constructor));
 /**
  * before_main - Prints a string before the
  *        main function is executed.
- * Retrun : void.
  */
-void before_main(void) __attribute__((constructor));
+
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-	I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
